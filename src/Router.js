07004d3ee3b7916f71595860531b7fd0110be5b1
Dave Harms <daveharmswebdev@gmail.com>
2017-01-30 11:14:41 -0600

    added create vital signs screen with navigation

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
+import { Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import Vitals from './components/Vitals';
+import VitalSignsCreate from './components/VitalSignsCreate';
 
 const RouterComponent = () => {
   return (
@@ -12,12 +14,13 @@ const RouterComponent = () => {
 
       <Scene key="main">
         <Scene
-          onRight={() => console.log('add vital sign!')}
+          onRight={() => Actions.vsCreate()}
           rightTitle="Add" 
           key="vitals" 
           component={Vitals} 
           title="Vital Signs" 
         />
+        <Scene key="vsCreate" component={VitalSignsCreate} title="New Vital Signs" />
       </Scene>
     </Router>
   );
